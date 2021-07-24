f405e32ee3aea61b8cc2cfd50f1db7e83512f901
AlPro <proningm@gmail.com>
2021-07-24 14:02:16 +0300

    Включаем и настраиваем оба слайдера в recommendation

--- src/js/slider.js
+++ src/js/slider.js
@@ -6,6 +6,10 @@ var slider = tns({
   controls: false,
   nav: false,
   fixedWidth: 285,
+  autoplay: true,
+  // autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
   responsive: {
     200: {
       fixedWidth: 183
@@ -17,7 +21,59 @@ var slider = tns({
       fixedWidth: 285
     },
     1800: {
-      fixedWidth: 'auto'
+      fixedWidth: 360
+    }
+  }
+});
+var slider = tns({
+  container: '.r-b-slide__list',
+  // items: 1,
+  loop: true,
+  mouseDrag: true,
+  controls: false,
+  nav: false,
+  edgePadding: 30,
+  autoplay: true,
+  // autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  autoplayButtonOutput: false,
+  responsive: {
+    200: {
+      items: 2
+    },
+    470: {
+      items: 3
+    },
+    768: {
+      items: 4
+    },
+    992: {
+      items: 5
+    },
+    1440: {
+      items: 6
+    },
+    1800: {
+      items: 7
+    }
+  }
+});
+var slider = tns({
+  container: '.r-t-slide__list',
+  mouseDrag: true,
+  controls: false,
+  nav: false,
+  items: 1,
+  center: true,
+  fixedWidth: 285,
+  startIndex: 1,
+  loop: true,
+  responsive: {
+    1440: {
+      fixedWidth: 350
+    },
+    1800: {
+      fixedWidth: 392
     }
   }
 });
