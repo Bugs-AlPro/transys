76c867333efad167f9298b846e103d985455c519
AlPro <proningm@gmail.com>
2021-08-01 13:07:48 +0300

    Включаем фиксацию header и изменение цвета при scroll #3

--- src/js/header.js
+++ src/js/header.js
@@ -5,17 +5,19 @@ let headerSvgTime = document.querySelector('.info-h__icon--time');
 let headerSvgPhone = document.querySelector('.info-h__icon--phone');
 
 window.onscroll = function () {
-  if (window.pageYOffset !== 0) {
-    headerMenu.classList.add('header-h--scroll');
-    headerLink.classList.add('info-h__link--scroll');
-    headerSvgLoc.classList.add('info-h__icon--scroll');
-    headerSvgTime.classList.add('info-h__icon--scroll');
-    headerSvgPhone.classList.add('info-h__icon--scroll');
-  } else {
+  if (window.pageYOffset == 0) {
     headerMenu.classList.remove('header-h--scroll');
     headerLink.classList.remove('info-h__link--scroll');
     headerSvgLoc.classList.remove('info-h__icon--scroll');
     headerSvgTime.classList.remove('info-h__icon--scroll');
     headerSvgPhone.classList.remove('info-h__icon--scroll');
+
+  } else {
+    headerMenu.classList.add('header-h--scroll');
+    headerLink.classList.add('info-h__link--scroll');
+    headerSvgLoc.classList.add('info-h__icon--scroll');
+    headerSvgTime.classList.add('info-h__icon--scroll');
+    headerSvgPhone.classList.add('info-h__icon--scroll');
+
   }
 };
